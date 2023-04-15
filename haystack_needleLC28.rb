edae51159f9d7c06b8a9f5286e01e2ddb51d7a23
Sanjay Prajapati <sprajapati@zeidlerlegalservices.com>
2023-04-15 09:44:18 +0530

    Added little imptoved solution

--- haystack_needleLC28.rb
+++ haystack_needleLC28.rb
@@ -2,25 +2,50 @@ require "pry"
 
 def str_str(haystack, needle)
   return -1 if haystack.nil? || needle.nil?
+  h_len = haystack.length
+  n_len = needle.length
+  return -1 if h_len < n_len
+  j = 0
+  k = 0
 
+  (0...h_len).each do |i|
+    k = i
+    j = 0
+    while j < n_len && needle[j] == haystack[k]
+      j += 1
+      k += 1
+    end
+    if j == n_len
+      return i
+    end
+  end
+  return -1
+end
+
+# Little bit improved solution by keeping track of match_found_at pointer
+def str_str1(haystack, needle)
+  return -1 if haystack.nil? || needle.nil?
   h = haystack.length
   n = needle.length
   return -1 if h < n
 
+  i=0
   j=0
-  k=0
-
-  (0...h).each do |i|
-    j = 0
-    k = i
-    while j < n && haystack[k] == needle[j]
-      j += 1
-      k += 1
+  match_found_at = 0
+  while i < haystack.length
+    if haystack[i] == needle[j]
+      match_found_at = i if j == 0
+      i+=1
+      j+=1
+    else
+      match_found_at = match_found_at+1
+      j=0
+      i=match_found_at
     end
-
-    return i if j == n
+    return match_found_at if j == n
   end
-  -1
+  return -1
 end
 
-puts str_str('xyzsad', 'sad')
+puts str_str('leetcode', 'leeto')
+
