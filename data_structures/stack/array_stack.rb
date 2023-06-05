6f73bb79e205edc6b7474a51036c51518cd67f55
prsanjay <lnvg530@gmail.com>
2023-06-05 08:32:08 +0530

    Implement stack using array

---
+++ data_structures/stack/array_stack.rb
@@ -0,0 +1,32 @@
+class ArrayStack
+  def initialize
+    @stack = Array.new
+  end
+
+  def push(data)
+    @stack.push(data)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek(index)
+    @stack[index]
+  end
+
+  def top
+    @stack.last
+  end
+
+  def is_empty?
+    @stack.empty?
+  end
+end
+
+stack = ArrayStack.new
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.pop
+stack.top
